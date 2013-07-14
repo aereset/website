54add196057a12e360fe44ef8e8e828251d36f62
Miguel Sánchez de León Peque <msdeleonpeque@gmail.com>
2013-07-14 15:04:50 +0200

    Set all admins permissions to '1'

--- doc/commands.sql
+++ doc/commands.sql
@@ -54,7 +54,9 @@ create table invitations(
 );
 
 --
--- The permissions table contains the users permissions.
+-- The permissions table contains the users permissions. The admin
+-- permissions (set bellow tables creation) should be modified if this
+-- table is changed!
 --
 create table permissions(
 	user int unsigned not null primary key,
@@ -83,5 +85,8 @@ create table user_info(
 -- Create admin user with password "admin" (not salted!).
 --
 insert into users (id,password) values ('admin','c7ad44cbad762a5da0a452f9e854fdc1e0e7a52a38015f23f3eab1d80b931dd472634dfac71cd34ebc35d16ab7fb8a90c81f975113d6c7538dc69dd8de9077ec');
-insert into permissions (user,admin) values ('1','1');
 
+--
+-- Set permissions to user "admin.
+--
+insert into permissions (user,admin,invitations,statistics,banners,wiki) values ('1','1','1','1','1','1');
